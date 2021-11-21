f60387eae0070ad6f8a609bae13e65b6b794548f
jordanbui23 <jordanbui20@gmail.com>
2021-11-21 13:49:48 -0600

    Created new buttons for the windows forms. Have to figure out how to scroll indefinitely to load all elements of followers and following

--- UserInterface.Designer.cs
+++ UserInterface.Designer.cs
@@ -24,13 +24,133 @@ namespace InstagramFollowerApp {
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserInterface));
+            this.uxLogIn = new System.Windows.Forms.Button();
+            this.uxFollowers = new System.Windows.Forms.TextBox();
+            this.uxUsername = new System.Windows.Forms.TextBox();
+            this.uxPassword = new System.Windows.Forms.TextBox();
+            this.uxCheckFollower = new System.Windows.Forms.Button();
+            this.uxUsernameInfo = new System.Windows.Forms.Label();
+            this.uxPasswordInfo = new System.Windows.Forms.Label();
+            this.uxFollowerButtonInfo = new System.Windows.Forms.Label();
+            this.uxAddFollower = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uxLogIn
+            // 
+            this.uxLogIn.Location = new System.Drawing.Point(53, 178);
+            this.uxLogIn.Name = "uxLogIn";
+            this.uxLogIn.Size = new System.Drawing.Size(198, 44);
+            this.uxLogIn.TabIndex = 0;
+            this.uxLogIn.Text = "Log In";
+            this.uxLogIn.UseVisualStyleBackColor = true;
+            this.uxLogIn.Click += new System.EventHandler(this.uxLogIn_Click);
+            // 
+            // uxFollowers
+            // 
+            this.uxFollowers.Location = new System.Drawing.Point(315, 12);
+            this.uxFollowers.Multiline = true;
+            this.uxFollowers.Name = "uxFollowers";
+            this.uxFollowers.ReadOnly = true;
+            this.uxFollowers.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.uxFollowers.Size = new System.Drawing.Size(287, 348);
+            this.uxFollowers.TabIndex = 1;
+            // 
+            // uxUsername
+            // 
+            this.uxUsername.Location = new System.Drawing.Point(12, 45);
+            this.uxUsername.Name = "uxUsername";
+            this.uxUsername.Size = new System.Drawing.Size(297, 31);
+            this.uxUsername.TabIndex = 2;
+            // 
+            // uxPassword
+            // 
+            this.uxPassword.Location = new System.Drawing.Point(12, 137);
+            this.uxPassword.Name = "uxPassword";
+            this.uxPassword.Size = new System.Drawing.Size(297, 31);
+            this.uxPassword.TabIndex = 3;
+            // 
+            // uxCheckFollower
+            // 
+            this.uxCheckFollower.Location = new System.Drawing.Point(73, 258);
+            this.uxCheckFollower.Name = "uxCheckFollower";
+            this.uxCheckFollower.Size = new System.Drawing.Size(160, 43);
+            this.uxCheckFollower.TabIndex = 6;
+            this.uxCheckFollower.Text = "Check Followers";
+            this.uxCheckFollower.UseVisualStyleBackColor = true;
+            this.uxCheckFollower.Click += new System.EventHandler(this.uxCheckFollower_Click);
+            // 
+            // uxUsernameInfo
+            // 
+            this.uxUsernameInfo.AutoSize = true;
+            this.uxUsernameInfo.Location = new System.Drawing.Point(12, 17);
+            this.uxUsernameInfo.Name = "uxUsernameInfo";
+            this.uxUsernameInfo.Size = new System.Drawing.Size(296, 25);
+            this.uxUsernameInfo.TabIndex = 7;
+            this.uxUsernameInfo.Text = "Username, Phone Number, Or Email";
+            // 
+            // uxPasswordInfo
+            // 
+            this.uxPasswordInfo.AutoSize = true;
+            this.uxPasswordInfo.Location = new System.Drawing.Point(102, 109);
+            this.uxPasswordInfo.Name = "uxPasswordInfo";
+            this.uxPasswordInfo.Size = new System.Drawing.Size(87, 25);
+            this.uxPasswordInfo.TabIndex = 8;
+            this.uxPasswordInfo.Text = "Password";
+            // 
+            // uxFollowerButtonInfo
+            // 
+            this.uxFollowerButtonInfo.AutoSize = true;
+            this.uxFollowerButtonInfo.Location = new System.Drawing.Point(24, 230);
+            this.uxFollowerButtonInfo.Name = "uxFollowerButtonInfo";
+            this.uxFollowerButtonInfo.Size = new System.Drawing.Size(260, 25);
+            this.uxFollowerButtonInfo.TabIndex = 9;
+            this.uxFollowerButtonInfo.Text = "Click Button Below If Logged In";
+            // 
+            // uxAddFollower
+            // 
+            this.uxAddFollower.Location = new System.Drawing.Point(12, 326);
+            this.uxAddFollower.Name = "uxAddFollower";
+            this.uxAddFollower.Size = new System.Drawing.Size(142, 34);
+            this.uxAddFollower.TabIndex = 10;
+            this.uxAddFollower.Text = "Add Follower";
+            this.uxAddFollower.UseVisualStyleBackColor = true;
+            this.uxAddFollower.Click += new System.EventHandler(this.uxAddFollower_Click);
+            // 
+            // UserInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(614, 372);
+            this.Controls.Add(this.uxAddFollower);
+            this.Controls.Add(this.uxFollowerButtonInfo);
+            this.Controls.Add(this.uxPasswordInfo);
+            this.Controls.Add(this.uxUsernameInfo);
+            this.Controls.Add(this.uxCheckFollower);
+            this.Controls.Add(this.uxPassword);
+            this.Controls.Add(this.uxUsername);
+            this.Controls.Add(this.uxFollowers);
+            this.Controls.Add(this.uxLogIn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "UserInterface";
+            this.Text = "Instagram Followers Form";
+            this.Load += new System.EventHandler(this.UserInterface_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button uxLogIn;
+        private System.Windows.Forms.TextBox uxFollowers;
+        private System.Windows.Forms.TextBox uxUsername;
+        private System.Windows.Forms.TextBox uxPassword;
+        private System.Windows.Forms.Button uxCheckFollower;
+        private System.Windows.Forms.Label uxUsernameInfo;
+        private System.Windows.Forms.Label uxPasswordInfo;
+        private System.Windows.Forms.Label uxFollowerButtonInfo;
+        private System.Windows.Forms.Button uxAddFollower;
     }
 }
 
--- UserInterface.cs
+++ UserInterface.cs
@@ -1,19 +1,125 @@
-﻿using System;
+﻿/* UserInterface.cs
+ * Author : Jordan Bui
+ * Start - November 19, 2021
+ */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace InstagramFollowerApp {
     public partial class UserInterface : Form {
         public UserInterface() {
             InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
         }
 
+        ChromeDriver drv; Thread thread;
+        string url = "https://www.instagram.com/accounts/login/?hl=en";
+        private void uxLogIn_Click(object sender, EventArgs e) {
+            string login = uxUsername.Text;
+            string pass = uxPassword.Text;
+            
+            if (login == "") {
+                MessageBox.Show("You have to enter a Phone Number, Username, or Email");
+            } else if (pass == "") {
+                MessageBox.Show("You have to enter a password");
+            } else {
+                thread = new Thread(LogIn); thread.Start();
+            }
 
+        }
+
+        /// <summary>
+        /// Loads up the chrome driver when the user starts the program
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UserInterface_Load(object sender, EventArgs e) {
+            // I had to install the chrome driver for my version of chrome. This might make it harder for others to use
+            drv = new ChromeDriver("C:/Users/jorda/OneDrive/Desktop/Instagram App/InstagramFollowerApp/chromedriver_win32");
+            drv.Navigate().GoToUrl(url);
+        }
+
+        /// <summary>
+        /// Logs the user in.
+        /// </summary>
+        private void LogIn() {
+            try {
+                drv.FindElements(By.XPath("//input[@class='_2hvTZ pexuQ zyHYP']"))[0].SendKeys(uxUsername.Text); Thread.Sleep(200);
+                //drv.FindElement(By.XPath("//button[@class='_4_yKc']")).Click();
+                drv.FindElements(By.XPath("//input[@class='_2hvTZ pexuQ zyHYP']"))[0].SendKeys(uxPassword.Text);
+                drv.FindElement(By.XPath("//button[@class='sqdOP  L3NKy   y3zKF     ']")).Click();
+
+                //MessageBox.Show(drv.Url); Used these to debug the log in
+                //drv.Url = "https://www.instagram.com/accounts/onetap/?next=%2F&hl=en";
+                //MessageBox.Show(drv.Url);
+                /*
+                if (drv.FindElements(By.XPath("//div[@class='pV7Qt         DPiy6    qF0y9          Igw0E     IwRSH      eGOV_         _4EzTm                                                                                                               qhGB0 ZUqME']")).Count > 0) {
+                    MessageBox.Show("Success");
+                } else {
+                    uxPassword.Text = "";
+                    uxUsername.Text = "";
+                    MessageBox.Show("Username or Password is incorrect");
+                }
+                */
+            } catch (Exception e) {
+                MessageBox.Show("Something went wrong. Try again.");
+            }
+        }
+
+        private void uxCheckFollower_Click(object sender, EventArgs e) {
+            try {
+                drv.FindElement(By.XPath("//span[@class='_2dbep qNELH']")).Click(); // Clicks the profile picture
+                drv.FindElement(By.XPath("//div[@class='_7UhW9   xLCgt      MMzan  KV-D4              fDxYl     ']")).Click(); // Clicks the profile button
+                drv.FindElements(By.XPath("//a[@class='-nal3 ']"))[0].Click(); // Clicks the followers
+                //drv.FindElements(By.XPath("//a[@class='-nal3 ']"))[1].Click(); // Clicks the following
+                // followers/following a class -nal3 
+                // following/followers li Y8-fY 
+                // follower specific FPmhX notranslate  _0imsa 
+                // follower specific FPmhX notranslate  _0imsa
+            } catch (Exception ee) {
+                MessageBox.Show("Something went wrong. Try re running the program.");
+            }
+ 
+        }
+
+        private void uxAddFollower_Click(object sender, EventArgs e) {
+            try {
+                IJavaScriptExecutor js = (IJavaScriptExecutor)drv;
+                MessageBox.Show(drv.FindElements(By.XPath("//span[@class='g47SY ']"))[0].Text); // The amount of followers
+                int SCROLL_PAUSE_TIME = 1;
+
+                // Get scroll height
+                string last_height = (string)js.ExecuteScript("return document.body.scrollHeight");
+
+                while (true) {
+                    // Scroll down to bottom
+                    js.ExecuteScript("window.scrollTo(0, document.body.scrollHeight);");
+
+                    // Wait to load page
+                    Thread.Sleep(SCROLL_PAUSE_TIME);
+
+                    // Calculate new scroll height and compare with last scroll height
+                    string new_height = (string)js.ExecuteScript("return document.body.scrollHeight");
+                    if (new_height == last_height)
+                        break;
+                    last_height = new_height;
+                }
+                MessageBox.Show(drv.FindElements(By.XPath("//a[@class='FPmhX notranslate  _0imsa ']"))[0].Text); // Gets the username
+                //Jv7Aj mArmR MqpiF  
+            } catch (Exception eee) {
+                MessageBox.Show("Something went wrong. SUCKS");
+            }
+
+        }
     }
 }
