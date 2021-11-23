5e76a46a2658f15b95e996b0fd6337c9f6576d23
jordanbui23 <jordanbui20@gmail.com>
2021-11-22 23:42:02 -0600

    FINALLY FINISHED THE PROJECT

--- UserInterface.Designer.cs
+++ UserInterface.Designer.cs
@@ -31,12 +31,16 @@ namespace InstagramFollowerApp {
             this.uxPassword = new System.Windows.Forms.TextBox();
             this.uxUsernameInfo = new System.Windows.Forms.Label();
             this.uxPasswordInfo = new System.Windows.Forms.Label();
-            this.uxClear = new System.Windows.Forms.Button();
+            this.uxFollowing = new System.Windows.Forms.TextBox();
+            this.uxFollowerLabel = new System.Windows.Forms.Label();
+            this.uxFollowingLabel = new System.Windows.Forms.Label();
+            this.uxLoaderLabel = new System.Windows.Forms.Label();
+            this.uxLoader = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // uxLogIn
             // 
-            this.uxLogIn.Location = new System.Drawing.Point(53, 209);
+            this.uxLogIn.Location = new System.Drawing.Point(56, 219);
             this.uxLogIn.Name = "uxLogIn";
             this.uxLogIn.Size = new System.Drawing.Size(198, 44);
             this.uxLogIn.TabIndex = 0;
@@ -46,7 +50,7 @@ namespace InstagramFollowerApp {
             // 
             // uxFollowers
             // 
-            this.uxFollowers.Location = new System.Drawing.Point(315, 12);
+            this.uxFollowers.Location = new System.Drawing.Point(315, 65);
             this.uxFollowers.Multiline = true;
             this.uxFollowers.Name = "uxFollowers";
             this.uxFollowers.ReadOnly = true;
@@ -56,22 +60,23 @@ namespace InstagramFollowerApp {
             // 
             // uxUsername
             // 
-            this.uxUsername.Location = new System.Drawing.Point(12, 45);
+            this.uxUsername.Location = new System.Drawing.Point(11, 100);
             this.uxUsername.Name = "uxUsername";
             this.uxUsername.Size = new System.Drawing.Size(297, 31);
             this.uxUsername.TabIndex = 2;
             // 
             // uxPassword
             // 
-            this.uxPassword.Location = new System.Drawing.Point(12, 137);
+            this.uxPassword.Location = new System.Drawing.Point(11, 172);
             this.uxPassword.Name = "uxPassword";
+            this.uxPassword.PasswordChar = '*';
             this.uxPassword.Size = new System.Drawing.Size(297, 31);
             this.uxPassword.TabIndex = 3;
             // 
             // uxUsernameInfo
             // 
             this.uxUsernameInfo.AutoSize = true;
-            this.uxUsernameInfo.Location = new System.Drawing.Point(12, 17);
+            this.uxUsernameInfo.Location = new System.Drawing.Point(13, 65);
             this.uxUsernameInfo.Name = "uxUsernameInfo";
             this.uxUsernameInfo.Size = new System.Drawing.Size(296, 25);
             this.uxUsernameInfo.TabIndex = 7;
@@ -80,28 +85,67 @@ namespace InstagramFollowerApp {
             // uxPasswordInfo
             // 
             this.uxPasswordInfo.AutoSize = true;
-            this.uxPasswordInfo.Location = new System.Drawing.Point(102, 109);
+            this.uxPasswordInfo.Location = new System.Drawing.Point(111, 144);
             this.uxPasswordInfo.Name = "uxPasswordInfo";
             this.uxPasswordInfo.Size = new System.Drawing.Size(87, 25);
             this.uxPasswordInfo.TabIndex = 8;
             this.uxPasswordInfo.Text = "Password";
             // 
-            // uxClear
+            // uxFollowing
             // 
-            this.uxClear.Location = new System.Drawing.Point(127, 299);
-            this.uxClear.Name = "uxClear";
-            this.uxClear.Size = new System.Drawing.Size(112, 34);
-            this.uxClear.TabIndex = 9;
-            this.uxClear.Text = "clear";
-            this.uxClear.UseVisualStyleBackColor = true;
-            this.uxClear.Click += new System.EventHandler(this.uxClear_Click);
+            this.uxFollowing.Location = new System.Drawing.Point(623, 65);
+            this.uxFollowing.Multiline = true;
+            this.uxFollowing.Name = "uxFollowing";
+            this.uxFollowing.ReadOnly = true;
+            this.uxFollowing.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.uxFollowing.Size = new System.Drawing.Size(287, 348);
+            this.uxFollowing.TabIndex = 10;
+            // 
+            // uxFollowerLabel
+            // 
+            this.uxFollowerLabel.AutoSize = true;
+            this.uxFollowerLabel.Location = new System.Drawing.Point(623, 27);
+            this.uxFollowerLabel.Name = "uxFollowerLabel";
+            this.uxFollowerLabel.Size = new System.Drawing.Size(245, 25);
+            this.uxFollowerLabel.TabIndex = 11;
+            this.uxFollowerLabel.Text = "Who doesn\'t follow you back\r\n";
+            // 
+            // uxFollowingLabel
+            // 
+            this.uxFollowingLabel.AutoSize = true;
+            this.uxFollowingLabel.Location = new System.Drawing.Point(315, 27);
+            this.uxFollowingLabel.Name = "uxFollowingLabel";
+            this.uxFollowingLabel.Size = new System.Drawing.Size(228, 25);
+            this.uxFollowingLabel.TabIndex = 12;
+            this.uxFollowingLabel.Text = "Who you don\'t follow back";
+            // 
+            // uxLoaderLabel
+            // 
+            this.uxLoaderLabel.AutoSize = true;
+            this.uxLoaderLabel.Location = new System.Drawing.Point(109, 344);
+            this.uxLoaderLabel.Name = "uxLoaderLabel";
+            this.uxLoaderLabel.Size = new System.Drawing.Size(81, 25);
+            this.uxLoaderLabel.TabIndex = 13;
+            this.uxLoaderLabel.Text = "Progress";
+            // 
+            // uxLoader
+            // 
+            this.uxLoader.Location = new System.Drawing.Point(67, 372);
+            this.uxLoader.Name = "uxLoader";
+            this.uxLoader.ReadOnly = true;
+            this.uxLoader.Size = new System.Drawing.Size(169, 31);
+            this.uxLoader.TabIndex = 14;
             // 
             // UserInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(614, 372);
-            this.Controls.Add(this.uxClear);
+            this.ClientSize = new System.Drawing.Size(922, 425);
+            this.Controls.Add(this.uxLoader);
+            this.Controls.Add(this.uxLoaderLabel);
+            this.Controls.Add(this.uxFollowingLabel);
+            this.Controls.Add(this.uxFollowerLabel);
+            this.Controls.Add(this.uxFollowing);
             this.Controls.Add(this.uxPasswordInfo);
             this.Controls.Add(this.uxUsernameInfo);
             this.Controls.Add(this.uxPassword);
@@ -110,7 +154,7 @@ namespace InstagramFollowerApp {
             this.Controls.Add(this.uxLogIn);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "UserInterface";
-            this.Text = "Instagram Followers Form";
+            this.Text = "Instagram Followers Tracker";
             this.Load += new System.EventHandler(this.UserInterface_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -125,7 +169,11 @@ namespace InstagramFollowerApp {
         private System.Windows.Forms.TextBox uxPassword;
         private System.Windows.Forms.Label uxUsernameInfo;
         private System.Windows.Forms.Label uxPasswordInfo;
-        private System.Windows.Forms.Button uxClear;
+        private System.Windows.Forms.TextBox uxFollowing;
+        private System.Windows.Forms.Label uxFollowerLabel;
+        private System.Windows.Forms.Label uxFollowingLabel;
+        private System.Windows.Forms.Label uxLoaderLabel;
+        private System.Windows.Forms.TextBox uxLoader;
     }
 }
 
--- UserInterface.cs
+++ UserInterface.cs
@@ -23,12 +23,16 @@ namespace InstagramFollowerApp {
             CheckForIllegalCrossThreadCalls = false;
         }
 
-
         ChromeDriver drv; Thread thread;
         private string url = "https://www.instagram.com/accounts/login/?hl=en";
 
         private ArrayList followers = new ArrayList();
         private ArrayList following = new ArrayList();
+        /// <summary>
+        /// Event handler for the log in button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void uxLogIn_Click(object sender, EventArgs e) {
             string login = uxUsername.Text;
             string pass = uxPassword.Text;
@@ -39,8 +43,6 @@ namespace InstagramFollowerApp {
                 MessageBox.Show("You have to enter a password");
             } else {
                 thread = new Thread(LogIn); thread.Start();
-                ArrayList notFollowing = CheckLists(followers, following);
-                ArrayList notFollowedBackBy = CheckLists(following, followers);
             }
 
         }
@@ -67,10 +69,15 @@ namespace InstagramFollowerApp {
                 Thread.Sleep(3500);
                 CheckFollower();
             } catch (Exception e) {
-                MessageBox.Show(e.Message);
+                drv.FindElements(By.XPath("//input[@class='_2hvTZ pexuQ zyHYP']"))[0].SendKeys(OpenQA.Selenium.Keys.Control + 'a' + OpenQA.Selenium.Keys.Backspace); Thread.Sleep(200);
+                drv.FindElements(By.XPath("//input[@class='_2hvTZ pexuQ zyHYP']"))[0].SendKeys(OpenQA.Selenium.Keys.Control + 'a' + OpenQA.Selenium.Keys.Backspace);
+                MessageBox.Show("Username or password is invalid");
             }
         }
 
+        /// <summary>
+        /// This method calls the rest of the methods to load the followers into arrayLists
+        /// </summary>
         private void CheckFollower() {
             try {
                 drv.FindElement(By.XPath("//span[@class='_2dbep qNELH']")).Click(); // Clicks the profile picture
@@ -80,39 +87,42 @@ namespace InstagramFollowerApp {
                 int numFollowers = Convert.ToInt32(drv.FindElements(By.XPath("//span[@class='g47SY ']"))[1].Text);
                 int numFollowing = Convert.ToInt32(drv.FindElements(By.XPath("//span[@class='g47SY ']"))[2].Text);
                 drv.FindElements(By.XPath("//a[@class='-nal3 ']"))[0].Click(); // Clicks the followers page
-                Thread.Sleep(2000); // Needs time for the followers page to load
-                ScrollDown();       // Scrolls down the followers
-                Thread.Sleep(2000);
+                Thread.Sleep(2000);                                            // Needs time for the followers page to load
+                ScrollDown(numFollowers);                                      // Scrolls down the followers
                 LoadPeople(followers, numFollowers);
-                Thread.Sleep(5000);
-                drv.FindElement(By.XPath("//svg[@class='_8-yf5 ']")).Click(); // Clicks the X
-                MessageBox.Show(followers.Count.ToString());
+                drv.FindElements(By.XPath("//div[@class='WaOAr']"))[1].Click();// Clicks the X
+                drv.FindElements(By.XPath("//a[@class='-nal3 ']"))[1].Click(); // Clicks the following page
+                Thread.Sleep(2000);                                            // Needs time for the followers page to load
+                ScrollDown(numFollowing);                                      // Scrolls down the followers
+                LoadPeople(following, numFollowing);
+                drv.FindElements(By.XPath("//div[@class='WaOAr']"))[1].Click();// Clicks the X
+                ArrayList notFollowing = CheckLists(followers, following);     // Fills an ArrayList with names of people who you don't follow back
+                ArrayList notFollowedBackBy = CheckLists(following, followers);
+                uxFollowers.Text = Names(notFollowing);
+                uxFollowing.Text = Names(notFollowedBackBy);
             } catch (Exception ee) {
-                MessageBox.Show(ee.Message);
+                drv.FindElements(By.XPath("//input[@class='_2hvTZ pexuQ zyHYP']"))[0].SendKeys(OpenQA.Selenium.Keys.Control + 'a' + OpenQA.Selenium.Keys.Backspace); Thread.Sleep(200);
+                drv.FindElements(By.XPath("//input[@class='_2hvTZ pexuQ zyHYP']"))[0].SendKeys(OpenQA.Selenium.Keys.Control + 'a' + OpenQA.Selenium.Keys.Backspace);
+                MessageBox.Show("Username or password is invalid");
             }
         }
 
         /// <summary>
         /// Scrolls down until it reaches the very bottom of the followers/following pages
         /// </summary>
-        private void ScrollDown() {
+        private void ScrollDown(int num) {
             try {
                 IWebElement scrollList = drv.FindElement(By.XPath("//ul[@class='jSC57  _6xe7A']"));    // Holds the element that has the height of the total followers
                 IJavaScriptExecutor js = drv; 
                 int oldHeight = scrollList.Size.Height;
-                Thread.Sleep(500);
                 js.ExecuteScript(String.Format("document.querySelector('div[class=\"isgrP\"').scrollTo(0, {0})", scrollList.Size.Height));
-                int sleepTime = 1100;
-                Thread.Sleep(1000);
-                while (oldHeight != scrollList.Size.Height) {
-                    Thread.Sleep(sleepTime);
-                    sleepTime += 65;
+                int totalHeight = (num * 54) - 54;                                           // Number of followers/following times the height of each bar
+                while (oldHeight <= totalHeight) {
                     js.ExecuteScript(String.Format("document.querySelector('div[class=\"isgrP\"').scrollTo(0, {0})", scrollList.Size.Height));
-                    Thread.Sleep(100);
                     oldHeight = scrollList.Size.Height;
                 }
-            } catch (Exception eeee) {
-                MessageBox.Show(eeee.Message);
+            } catch (Exception except) {
+                MessageBox.Show("Something went wrong when scrolling down. Try restarting the program");
             }
         }
 
@@ -123,19 +133,17 @@ namespace InstagramFollowerApp {
         /// <param name="people">How many people it should add</param>
         private void LoadPeople(ArrayList array, int people) {
             for (int i = 0; i < people; i++) {
+                uxLoader.Text = String.Format("{0}/{1} loaded", i + 1, people);
                 array.Add(drv.FindElements(By.XPath("//a[@class='FPmhX notranslate  _0imsa ']"))[i].Text);
             }
         }
 
-        private void uxClear_Click(object sender, EventArgs e) {
-            try {
-                drv.FindElements(By.XPath("//input[@class='_2hvTZ pexuQ zyHYP']"))[0].Clear();
-                drv.FindElements(By.XPath("//input[@class='_2hvTZ pexuQ zyHYP']"))[1].Clear();
-            } catch (Exception fd) {
-                MessageBox.Show(fd.Message);
-            }
-        }
-
+        /// <summary>
+        /// Compars the arraylists to see who isn't in the other one
+        /// </summary>
+        /// <param name="first">First ArrayList being compared</param>
+        /// <param name="second">Second ArrayList being compared</param>
+        /// <returns>An ArrayList with names of people who are in the first list but not the second list</returns>
         private ArrayList CheckLists(ArrayList first, ArrayList second) {
             // Make this more efficient
             ArrayList names = new ArrayList();
@@ -144,6 +152,19 @@ namespace InstagramFollowerApp {
                     names.Add(first[i]);
             }
             return names;
+        }
+
+        /// <summary>
+        /// Formats the names in the list 
+        /// </summary>
+        /// <param name="list">The ArrayList holding the names</param>
+        /// <returns>Formatted string</returns>
+        private string Names(ArrayList list) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < list.Count; i++) {
+                sb.AppendLine(list[i].ToString());
+            }
+            return sb.ToString();
         }
     }
 }
